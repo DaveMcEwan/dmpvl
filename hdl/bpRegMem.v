4a5ab1e5ff7bb5de442775f276cc779ba278b217
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-04-23 13:47:52 +0100

    bpRegMem Simplify dffs.

--- hdl/bpRegMem.v
+++ hdl/bpRegMem.v
@@ -59,12 +59,8 @@ always @*
   else              rd_d = rd_q;
 
 localparam ADDR_W = $clog2(N_REG);
-`dff_nocg_srst(reg [ADDR_W-1:0], addr, i_clk, i_rst, '0)
-always @*
-  if (i_cg && txnBegin)
-    addr_d = i_bp_data[ADDR_W-1:0];
-  else
-    addr_d = addr_q;
+`dff_cg_srst(reg [ADDR_W-1:0], addr, i_clk, i_cg && txnBegin, i_rst, '0)
+always @* addr_d = i_bp_data[ADDR_W-1:0];
 
 // Track validity of address to return zeros for out-of-range.
 wire addrInRange;
@@ -73,28 +69,18 @@ generate if (ADDR_W == 'd7) begin
 end else begin
   localparam N_REG_= N_REG;
 
-  `dff_nocg_srst(reg, addrInRange, i_clk, i_rst, '0)
-  always @*
-    if (i_cg && txnBegin)
-      addrInRange_d = (i_bp_data[6:0] < N_REG_[6:0]);
-    else
-      addrInRange_d = addrInRange_q;
+  `dff_cg_srst(reg, addrInRange, i_clk, i_cg && txnBegin, i_rst, '0)
+  always @* addrInRange_d = (i_bp_data[6:0] < N_REG_[6:0]);
 
   assign addrInRange = addrInRange_q;
 end endgenerate
 
-(* mem2reg *) reg [7:0] memory_d [N_REG];
 (* mem2reg *) reg [7:0] memory_q [N_REG]; // dff_cg_norst
 genvar i;
 generate for (i = 0; i < N_REG; i=i+1) begin : reg_b
-  always @*
-    if (i_cg && wrEnd && (addr_q == i) && addrInRange)
-      memory_d[i] = i_bp_data;
-    else
-      memory_d[i] = memory_q[i];
-
   always @(posedge i_clk)
-      memory_q[i] <= memory_d[i];
+    if (i_cg && wrEnd && (addr_q == i) && addrInRange)
+      memory_q[i] <= i_bp_data;
 end : reg_b endgenerate
 
 `dff_cg_srst(reg [7:0], rdData, i_clk, i_cg && rdBegin, i_rst, '0)
