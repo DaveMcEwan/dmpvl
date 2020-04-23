db920767d79edf7d9cdd972e7c239e9ba6e04ace
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-04-23 15:43:16 +0100

    testBytePipe Move common general BytePipe types and functions into dmppl.

--- prj/usbfsBpRegMem_TinyFPGA-BX/testBytePipe.py
+++ prj/usbfsBpRegMem_TinyFPGA-BX/testBytePipe.py
@@ -41,34 +41,10 @@ import serial
 
 # git clone https://github.com/DaveMcEwan/dmppl.git && pip install -e dmppl
 from dmppl.base import run, verb, dbg
+from dmppl.bytePipe import BpAddrs, BpAddrValues, BpMem, \
+    bpReadSequential, bpWriteSequential, bpPrintMem, bpAddrValuesToMem
 
 __version__ = "0.1.0"
-
-# {{{ types
-
-BpAddrs = Sequence[int]
-#BpAddrValues = Tuple[Tuple[int, int], ...]
-BpAddrValues = Sequence[Tuple[int, int]]
-
-# Specific type for the values of the whole addressable range (128B).
-BpMem = Tuple[int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int,
-              int, int, int, int, int, int, int, int]
-
-# }}} types
 
 def getBitfilePath(argBitfile) -> str: # {{{
 
@@ -112,98 +88,6 @@ def uploadBitfile(bitfile): # {{{
     return p.returncode
 # }}} def uploadBitfile
 
-def bpAddrValuesToMem(addrValues:BpAddrValues) -> BpMem: # {{{
-
-    ret_:List[int] = [-1]*128
-    for addr,value in addrValues:
-        assert isinstance(addr, int), (type(addr), addr)
-        assert 0 <= addr < 128, addr
-        assert isinstance(value, int), (type(value), value)
-        assert 0 <= value < 256, value
-        ret_[addr] = value
-
-    assert isinstance(ret_, list), (type(ret_), ret_)
-    assert 128 == len(ret_), (len(ret_), ret_)
-    ret:BpMem = cast(BpMem, tuple(ret_))
-
-    assert isinstance(ret, tuple), (type(ret), ret)
-    assert 128 == len(ret), (len(ret), ret)
-
-    return ret
-# }}} def bpAddrValuesToMem
-
-def bpReadSequential(device, addrs:BpAddrs) -> BpAddrValues: # {{{
-
-    ret_ = []
-    for i,addr in enumerate(addrs):
-
-        # Send read command
-        assert isinstance(addr, int), (type(addr), addr)
-        assert 0 <= addr < 128, (i, addr)
-        assert 1 == device.write(bytes([addr]))
-
-        value:int = ord(device.read(1))
-
-        # First return value is discarded.
-        if 0 == i:
-            continue
-        else:
-            ret_.append((addrs[i-1], value))
-
-    # Last address is sent again,
-    assert 1 == device.write(bytes([addrs[-1]]))
-    ret_.append((addrs[-1], ord(device.read(1))))
-
-    # Finalize return value.
-    ret = tuple(ret_)
-
-    assert len(ret) == len(addrs), (ret, addrs)
-    assert all((a == ra) for a,(ra,rv) in zip(addrs, ret))
-    assert all((rv < 256) for ra,rv in ret)
-
-    return ret
-# }}} def bpReadSequential
-
-def bpWriteSequential(device, addrValues:BpAddrValues) -> BpAddrValues: # {{{
-
-    ret_ = []
-    for i,(addr,value) in enumerate(addrValues):
-
-        # Send write command
-        assert isinstance(addr, int), (type(addr), addr)
-        assert 0 <= addr < 128, (i, addr)
-        assert 2 == device.write(bytes([addr + 128, value]))
-
-        ret_.append((addr, ord(device.read(1))))
-
-    # Finalize return value.
-    ret:BpAddrValues = tuple(ret_)
-
-    assert len(ret) == len(addrValues), (ret, addrValues)
-    assert all((a == ra) for (a,v),(ra,rv) in zip(addrValues, ret))
-    assert all((rv < 256) for ra,rv in ret)
-
-    return ret
-# }}} def bpWriteSequential
-
-def printBpMem(title:str, mem:BpMem) -> None: # {{{
-
-    print(title.strip() + ':')
-
-    # Increasing left-to-right and top-to-bottom.
-    # Text of 16 lines of 8 bytes.
-    for i in range(16):
-        base = i*8
-        values = [mem[base + offset] for offset in range(8)]
-
-        line = ' '.join("--" if v < 0 else ("%02x" % v) \
-                        for v in values)
-
-        print("  %3d .. %3d: %s" % (base, base+7, line))
-
-    return
-# }}} def printBpMem
-
 def actionBits(rd, wr, mem): # {{{
     verb("Writing ones to all register locations...", end='')
     _ = mem( wr(list((addr, 0xff) for addr in range(128))) )
@@ -219,7 +103,7 @@ def actionBits(rd, wr, mem): # {{{
     symdiff:BpMem = cast(BpMem, tuple(o ^ z for o,z in zip(ones, zeros)))
     verb("Done")
 
-    printBpMem("Writable bits", symdiff)
+    bpPrintMem("Writable bits", symdiff)
 
     return # No return value
 # }}} def actionBits
@@ -229,7 +113,7 @@ def actionDump(rd, wr, mem): # {{{
     init0:BpMem = mem( rd(list(range(128))) )
     verb("Done")
 
-    printBpMem("Dump", init0)
+    bpPrintMem("Dump", init0)
 
     return # No return value
 # }}} def actionDump
@@ -238,12 +122,12 @@ def actionTest(rd, wr, mem): # {{{
     verb("Reading all register locations...", end='')
     init0:BpMem = mem( rd(list(range(128))) )
     verb("Done")
-    printBpMem("Initial values", init0)
+    bpPrintMem("Initial values", init0)
 
     verb("Writing ones to all register locations...", end='')
     init1:BpMem = mem( wr(list((addr, 0xff) for addr in range(128))) )
     verb("Done")
-    printBpMem("Initial values (again)", init1)
+    bpPrintMem("Initial values (again)", init1)
     verb("Checking previous unchanged...", end='')
     assert all((i0 == i1) for i0,i1 in zip(init0, init1))
     verb("Done")
@@ -251,22 +135,22 @@ def actionTest(rd, wr, mem): # {{{
     verb("Writing zeros to all register locations...", end='')
     ones:BpMem = mem( wr(list((addr, 0x00) for addr in range(128))) )
     verb("Done")
-    printBpMem("Ones", ones)
+    bpPrintMem("Ones", ones)
 
     verb("Reading all register locations...", end='')
     zeros:BpMem = mem( rd(list(range(128))) )
     verb("Checking writable bits...", end='')
     symdiff:BpMem = cast(BpMem, tuple(o ^ z for o,z in zip(ones, zeros)))
     verb("Done")
-    printBpMem("Zeros", zeros)
-    printBpMem("Writable bits", symdiff)
+    bpPrintMem("Zeros", zeros)
+    bpPrintMem("Writable bits", symdiff)
 
     verb("Writing unique values to all register locations...", end='')
     _ = mem( wr(list((addr, addr+10) for addr in range(128))) )
     verb("Reading back...", end='')
     addrPlus10:BpMem = mem( rd(list(range(128))) )
     verb("Done")
-    printBpMem("mem[addr] <-- (addr+10)", addrPlus10)
+    bpPrintMem("mem[addr] <-- (addr+10)", addrPlus10)
 
     return # No return value
 # }}} def actionTest
