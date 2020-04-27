5ccaddffd66edca29a2de3eb1cb6011b7a5837e9
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-04-27 13:07:10 +0100

    bgRegMem More tidy prep.

--- hdl/bpRegMem.v
+++ hdl/bpRegMem.v
@@ -9,14 +9,16 @@
      expected it to be as you update (functional monitoring).
   4. Efficient polling loops for non-linear sequences of addresses.
 
-- All transactions generate 1B response.
 - All addressable locations are 1B wide.
 - Address is up to 7b wide (up to 128 positions).
 - Non-implemented addresses always read as 0.
   This allows all writeable bits to be discovered by writing `0xff` to each
   position, inspecting the result.
+- All non-burst transactions generate 1B response.
 - Write transaction sends cmd+address byte, then data byte, then receives 1B
   with previous/overwritten value at that address.
+  The received value serves as a write-response to confirm that the write has
+  taken place.
 - Read transaction sends cmd+address byte, then receives 1B with the value at
   the previous address.
 */
@@ -39,18 +41,22 @@ module bpRegMem #(
 
 localparam ADDR_W = $clog2(N_REG);
 
-`dff_cg_srst(reg, wr, i_clk, i_cg, i_rst, '0)
-`dff_cg_srst(reg, rd, i_clk, i_cg, i_rst, '0)
+`dff_cg_srst(reg, wr, i_clk, i_cg, i_rst, '0) // 1b FSM
+`dff_cg_srst(reg, rd, i_clk, i_cg, i_rst, '0) // 1b FSM
 `dff_cg_srst(reg [ADDR_W-1:0], addr, i_clk, i_cg, i_rst, '0)
 `dff_cg_srst(reg [7:0], rdData, i_clk, i_cg, i_rst, '0)
 
+// IO aliases
 wire in_accepted = i_bp_valid && o_bp_ready;
 wire out_accepted = o_bp_valid && i_bp_ready;
+wire cmdWr = i_bp_data[7];
+wire cmdRd = !i_bp_data[7];
+wire [6:0] cmdAddr = i_bp_data[6:0];
 
 wire txnBegin = !wr_q && in_accepted;
-wire wrSet = txnBegin && i_bp_data[7];
+wire wrSet = txnBegin && cmdWr;
 wire wrClr = wr_q && in_accepted;
-wire rdSet = (txnBegin && !wrSet) || wrClr;
+wire rdSet = (txnBegin && cmdRd) || wrClr;
 wire rdClr = out_accepted;
 
 always @*
@@ -64,7 +70,7 @@ always @*
   else            rd_d = rd_q;
 
 always @* addr_d = txnBegin ?
-  i_bp_data[ADDR_W-1:0] :
+  cmdAddr[ADDR_W-1:0] :
   addr_q;
 
 // Track validity of address to return zeros for out-of-range.
@@ -74,8 +80,10 @@ generate if ((N_REG == 'd128) || (ZERO_UNIMPL == 0)) begin
 end else begin
   localparam ADDR_HI = N_REG-1;
 
-  `dff_cg_srst(reg, addrInRange, i_clk, i_cg && txnBegin, i_rst, '0)
-  always @* addrInRange_d = (ADDR_HI[6:0] >= i_bp_data[6:0]);
+  `dff_cg_srst(reg, addrInRange, i_clk, i_cg, i_rst, '0)
+  always @* addrInRange_d = txnBegin ?
+    (ADDR_HI[6:0] >= cmdAddr) :
+    addrInRange_q;
 
   assign addrInRange = addrInRange_q;
 end endgenerate
