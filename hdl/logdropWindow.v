9321f500451a5a17e05351a407d3065a9d697f9e
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-05-04 20:16:29 +0100

    logdropWindow Abstract model translated from Python.

--- hdl/logdropWindow.v
+++ hdl/logdropWindow.v
@@ -1,5 +1,5 @@
 
-/* WIP/UNTESTED windowing function logdrop.
+/* WIP/UNTESTED Windowing function "logdrop".
 t ∈ [0, n); w(t) = 2**(⌊log₂min(t, n-t-1)⌋ - log₂(n/4))
 x(t) ∈ (0, 1]; y(t) = w(t) x(t);
 
@@ -8,13 +8,16 @@ semantics are slightly different so it's not a completely 1-to-1 mapping.
 The main difference is that Q-format with 0 unit bits and k fraction bits
 represents the range [0, 1), whereas fx-format represents (0, 1].
 E.g.
-  1.0 (decimal) = 1.000 (Q-format) ~= .1111 (fx-format)
-  0.0 (decimal) = 0.000 (Q-format) ~= .0000 (fx-format)
-  0.125 (decimal) 0.001 (Q-format) ~= .0000 (fx-format)
-  0.25 (decimal) 0.010 (Q-format) ~= .0011 (fx-format)
-  0.5 (decimal) 0.100 (Q-format) ~= .0111 (fx-format)
 
-Inputs must be in fx-format, meaning that '0 is the smallest
+  Decimal   Q-format Bits   Q-format Range    fx-format Bits    fx-format Range
+  -------   -------------   --------------    --------------    ---------------
+  1.0       1.000           [0.9375, 1)       .1111             (0.9375, 1]
+  0.0       0.000           [0, 0.125)        .0000             (0, 0.125]
+  0.125     0.001           [0, 0.125)        .0000             (0, 0.125]
+  0.25      0.010           [0.1875, 0.25)    .0011             (0.1875, 0.25]
+  0.5       0.100           [0.4375, 0.25)    .0111             (0.4375, 0.25]
+
+Inputs may be in either Q-format or fx-format, meaning that '0 is the smallest
 representable number, and '1 is the largest representable number.
 */
 module logdropWindow #(
@@ -29,6 +32,40 @@ module logdropWindow #(
 
   output wire [DATA_W-1:0]              o_y
 );
+generate if (ABSTRACT_MODEL) begin
+`ifndef SYNTHESIS
+  // Translation from Python plotting script.
+
+  int t, x, y, n, onehotIdxN, a, shft;
+
+  always @* begin
+    x = i_x;
+    n = WINLEN;
+
+    // NOTE: Use the "proper math" notation (1-indexed) instead of "comp-sci"
+    // notation (0-indexed).
+    t = i_t + 1;
+
+    // n must be a power of 2 so only a single bit set.
+    // The power is the index of the set bit.
+    onehotIdxN = $clog2(n/2);
+
+    // min(t, WINLEN-t-1) increments then decrements with t.
+    // Use a sawtooth counter, or opposing up/down counters.
+    if (t <= n / 2)
+        a = t;
+    else
+        a = n - t + 1;
+
+    shft = onehotIdxN - $clog2(a);
+
+    y = x >> shft;
+  end
+
+  assign o_y = y;
+
+`endif
+end else begin
 
 localparam WINLEN_W = $clog2(WINLEN); // 16 --> 4
 
@@ -54,9 +91,11 @@ fxcs #(
 //  - Largest w(t) multiplier (smallest shift), is 1 (>> 0).
 localparam SHIFT_W = $clog2(WINLEN_W); // 4 --> 2
 localparam WINLEN_W_M1_ = WINLEN_W-1;
-wire [SHIFT_W-1:0] shft = centerHalf ? '0 : WINLEN_W_M1_[SHIFT_W-1:0] - flog2a;
+wire [SHIFT_W-1:0] shft = WINLEN_W_M1_[SHIFT_W-1:0] - flog2a;
 
 // Multiply inputs by w(t).
-assign o_y = i_x >> shft;
+assign o_y = centerHalf ? i_x : (i_x >> shft);
+
+end endgenerate
 
 endmodule
