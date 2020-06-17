2422ab70d1146513a8d371068521d2964a278719
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-06-17 18:42:30 +0100

    tb/correlator First hookup to correlator.py NONFUNCTIONAL

---
+++ tb/correlator/run.sh
@@ -0,0 +1,23 @@
+
+# Start the tb as a background process which creates the fifo "tbCtrl" then
+# listens to it for commands.
+./build/Vcorrelator_tb > ./build/run.log &
+sleep 2
+
+# Initialize the design to look like it has come out of reset for a few cycles.
+echo "reset" > tbCtrl
+echo "step 10" > tbCtrl
+
+# Let the design run freely at a slow clockrate.
+# This sets the period between clock ticks to a fixed number of microseconds,
+# and execution time for evaluating each tick is ignored, so actual frequency
+# will be slightly (or a lot) less.
+echo "frequency_Hz 100" > tbCtrl
+echo "continue" > tbCtrl
+
+# Run the interactive application as a foreground process.
+../../prj/correlator/correlator.py --no-prog --device=ptyBytePipe_bp0
+
+# Now that the application has finished, stop the tb.
+echo "quit" > tbCtrl
+
