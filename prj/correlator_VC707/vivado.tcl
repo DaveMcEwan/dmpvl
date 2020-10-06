b7884db159e9b0d7eee504c145d4931ae70b50ef
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-10-06 18:01:53 +0100

    correlator_VC707 vivado.tcl getopt

--- prj/correlator_VC707/vivado.tcl
+++ prj/correlator_VC707/vivado.tcl
@@ -1,7 +1,40 @@
 
-set REPORT 1
-set CHECKPOINT 1
-set NETLIST 1
+# getopt() usage:
+#   Option with default value:
+#     getopt argv <shName> <tclName> <defaultValue>
+#   OR
+#   Boolean flag:
+#     set <tclName> [getopt argv <shName>]
+#   E.g.
+#     $ vivado -mode tcl -tclargs --foo 123 -a
+#       % getopt argv --foo abc 456
+#       % puts $abc                     --> "123"
+#       % getopt argv --bar xyz 789
+#       % puts $xyz                     --> "789"
+#       % set $boolA [getopt argv a]
+#       % puts $boolA                   --> "1"
+#       % set $boolB [getopt argv b]
+#       % puts $boolB                   --> "0"
+# https://wiki.tcl-lang.org/page/getopt
+proc getopt {_argv name {_var ""} {default ""}} {
+  upvar 1 $_argv argv $_var var
+  set pos [lsearch -regexp $argv ^$name]
+  if {$pos>=0} {
+    set to $pos
+    if {$_var ne ""} {
+      set var [lindex $argv [incr to]]
+    }
+    set argv [lreplace $argv $pos $to]
+    return 1
+  } else {
+    if {[llength [info level 0]] == 5} {set var $default}
+      return 0
+  }
+}
+
+getopt argv --report        REPORT      1
+getopt argv --checkpoint    CHECKPOINT  1
+getopt argv --netlist       NETLIST     1
 
 # AC701 (Artix-7)
 #set part "xc7a200tfbg676-2"
