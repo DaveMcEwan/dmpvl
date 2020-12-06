b11434a0861e27c54146ed0657bb4bc53c81f4fa
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-12-06 21:58:18 +0000

    cdcFifo Separate read and write domains, slowSmall/fastLarge topology parameter on individual sides.

--- hdl/cdcFifo.sv
+++ hdl/cdcFifo.sv
@@ -13,8 +13,12 @@ module cdcFifo #(
   parameter DEPTH = 8,  // >= 2, must be power-of-2.
   parameter FLOPS_NOT_MEM = 0,
 
-  // 0 -> gray-->bin-->incr-->gray in single cycle.
-  // 1 -> bin-->incl, bin-->gray separate FFs with result 1-cycle later.
+  // slowSmall: gray-->bin-->incr-->gray in single cycle.
+  // fastLarge: bin-->incl, bin-->gray separate FFs with result 1-cycle later.
+  // 0 -> slowSmall on both W and R sides.
+  // 1 -> fastLarge on W, slowSmall on R.
+  // 2 -> slowSmall on W, fastLarge on R.
+  // 3 -> fastLarge on both W and R sides.
   parameter TOPOLOGY = 0
 ) (
   input  wire                         i_wclk,
@@ -44,21 +48,21 @@ genvar i;
 
 localparam PTR_W = $clog2(DEPTH);
 
-wire wEn, rEn;
+wire wEn;
 wire [WIDTH-1:0] wdata, rdata;
-wire [PTR_W:0] wptrBin, rptrBin;
-wire [PTR_W:0] wptrSyncedGray, rptrSyncedGray;
-wire [PTR_W:0] wptrSyncedBin, rptrSyncedBin;
 `dff_cg_srst(reg [PTR_W:0], wptrGray, i_wclk, i_wcg, i_wrst, '0)
 `dff_cg_srst(reg [PTR_W:0], rptrGray, i_rclk, i_rcg, i_rrst, '0)
 
 assign o_wpushed = o_wready && i_wvalid;
 assign o_rpopped = o_rvalid && i_rready;
 
-// TODO: Separate TOPOLOGY for write and read sides.
-generate if (TOPOLOGY) begin
+generate if (1) begin: wDomain
+  wire [PTR_W:0] wptrBin, rptrSyncedGray, rptrSyncedBin;
+  wire ptrsUnequalW = (wptrBin`LSb(PTR_W) != rptrSyncedBin`LSb(PTR_W));
+  wire ptrsWrappedW = (wptrBin[PTR_W] != rptrSyncedBin[PTR_W]);
 
-  if (1) begin: wDomain
+  if ((TOPOLOGY & 1) != 0) begin : fastLargeW
+
     `dff_cg_srst(reg, wEn, i_wclk, i_wcg, i_wrst, 1'b0)
     always @* wEn_d = o_wpushed;
     assign wEn = wEn_q;
@@ -82,18 +86,64 @@ generate if (TOPOLOGY) begin
     assign wdata = wdata_q;
 
     assign o_wready = ptrsUnequalW || !ptrsWrappedW; // !full
-  end : wDomain
 
-  if (1) begin: rDomain
+  end : fastLargeW else begin : slowSmallW
+
+    assign wEn = o_wpushed;
+
+    wire [PTR_W:0] b2gWptr;
+    binToGray #(.WIDTH(PTR_W+1)) u_b2gWptr (.i_bin(wptrBin + 'd1), .o_gray(b2gWptr));
+    always @* wptrGray_d = wEn ? b2gWptr : wptrGray_q;
+
+    grayToBin #(.WIDTH(PTR_W+1)) u_g2bWptr (.i_gray(wptrGray_q), .o_bin(wptrBin));
+    assign o_wptr = wptrBin`LSb(PTR_W);
+
+    grayToBin #(.WIDTH(PTR_W+1)) u_g2bRsynced (.i_gray(rptrSyncedGray), .o_bin(rptrSyncedBin));
+
+    assign wdata = i_wdata;
+    assign o_wready = ptrsUnequalW || !ptrsWrappedW; // !full
+
+  end : slowSmallW
+
+  for (i = 0; i <= PTR_W; i=i+1) begin
+    syncBit #(
+      .DEBOUNCE_CYCLES  (0),
+      .EDGECNTR_W       (1),
+      .N_SYNC           (2)
+    ) syncBit_rptr (
+      .i_clk      (i_wclk),
+      .i_cg       (i_wcg),
+      .i_rst      (i_wrst),
+
+      .i_bit      (rptrGray_q[i]),
+
+      .o_bit      (rptrSyncedGray[i]),
+      .o_edge     (),
+      .o_rise     (),
+      .o_fall     (),
+      .o_nEdge    (),
+      .o_nRise    (),
+      .o_nFall    ()
+    );
+  end
+end : wDomain endgenerate
+
+generate if (1) begin: rDomain
+  wire [PTR_W:0] rptrBin, wptrSyncedGray, wptrSyncedBin;
+  wire ptrsUnequalR = (rptrBin`LSb(PTR_W) != wptrSyncedBin`LSb(PTR_W));
+  wire ptrsWrappedR = (rptrBin[PTR_W] != wptrSyncedBin[PTR_W]);
+
+  if ((TOPOLOGY & 2) != 0) begin : fastLargeR
+
     `dff_cg_srst(reg, rvalid, i_rclk, i_rcg, i_rrst, 1'b0)
     assign o_rvalid = rvalid_q;
-    assign rEn = ptrsUnequalR || ptrsWrappedR; // !empty
+    wire notEmpty = ptrsUnequalR || ptrsWrappedR;
     always @*
-      if (rEn)           rvalid_d = 1'b1;
+      if (notEmpty)      rvalid_d = 1'b1;
       else if (i_rready) rvalid_d = 1'b0;
       else               rvalid_d = rvalid_q;
 
-    wire updateData = rEn && (!rvalid_q || i_rready);
+    wire updateData = notEmpty && (!rvalid_q || i_rready);
     `dff_cg_norst(reg [WIDTH-1:0], rdata, i_rclk, i_rcg)
     always @* rdata_d = updateData ? rdata : rdata_q;
     assign o_rdata = rdata_q;
@@ -108,38 +158,46 @@ generate if (TOPOLOGY) begin
     `dff_cg_srst(reg [PTR_W:0], wptrSyncedBin, i_rclk, i_rcg, i_rrst, '0)
     grayToBin #(.WIDTH(PTR_W+1)) u_g2bWsynced (.i_gray(wptrSyncedGray), .o_bin(wptrSyncedBin_d));
     assign wptrSyncedBin = wptrSyncedBin_q;
-  end : rDomain
 
-end else begin
-  assign wEn = o_wpushed;
-  assign rEn = o_rpopped;
+  end : fastLargeR else begin : slowSmallR
 
-  wire [PTR_W:0] b2gWptr, b2gRptr;
-  binToGray #(.WIDTH(PTR_W+1)) u_b2gWptr (.i_bin(wptrBin + 'd1), .o_gray(b2gWptr));
-  binToGray #(.WIDTH(PTR_W+1)) u_b2gRptr (.i_bin(rptrBin + 'd1), .o_gray(b2gRptr));
-  always @* wptrGray_d = wEn ? b2gWptr : wptrGray_q;
-  always @* rptrGray_d = rEn ? b2gRptr : rptrGray_q;
+    wire [PTR_W:0] b2gRptr;
+    binToGray #(.WIDTH(PTR_W+1)) u_b2gRptr (.i_bin(rptrBin + 'd1), .o_gray(b2gRptr));
+    always @* rptrGray_d = o_rpopped ? b2gRptr : rptrGray_q;
 
-  grayToBin #(.WIDTH(PTR_W+1)) u_g2bWptr (.i_gray(wptrGray_q), .o_bin(wptrBin));
-  grayToBin #(.WIDTH(PTR_W+1)) u_g2bRptr (.i_gray(rptrGray_q), .o_bin(rptrBin));
+    grayToBin #(.WIDTH(PTR_W+1)) u_g2bRptr (.i_gray(rptrGray_q), .o_bin(rptrBin));
+    assign o_rptr = rptrBin`LSb(PTR_W);
 
-  assign o_wptr = wptrBin`LSb(PTR_W);
-  assign o_rptr = rptrBin`LSb(PTR_W);
+    grayToBin #(.WIDTH(PTR_W+1)) u_g2bWsynced (.i_gray(wptrSyncedGray), .o_bin(wptrSyncedBin));
 
-  grayToBin #(.WIDTH(PTR_W+1)) u_g2bWsynced (.i_gray(wptrSyncedGray), .o_bin(wptrSyncedBin));
-  grayToBin #(.WIDTH(PTR_W+1)) u_g2bRsynced (.i_gray(rptrSyncedGray), .o_bin(rptrSyncedBin));
+    assign o_rdata = rdata;
+    assign o_rvalid = ptrsUnequalR || ptrsWrappedR; // !empty
 
-  assign wdata = i_wdata;
-  assign o_rdata = rdata;
+  end : slowSmallR
 
-  assign o_wready = ptrsUnequalW || !ptrsWrappedW; // !full
-  assign o_rvalid = ptrsUnequalR || ptrsWrappedR; // !empty
-end endgenerate
+  for (i = 0; i <= PTR_W; i=i+1) begin
+    syncBit #(
+      .DEBOUNCE_CYCLES  (0),
+      .EDGECNTR_W       (1),
+      .N_SYNC           (2)
+    ) syncBit_wptr (
+      .i_clk      (i_rclk),
+      .i_cg       (i_rcg),
+      .i_rst      (i_rrst),
 
-wire ptrsUnequalW = (wptrBin`LSb(PTR_W) != rptrSyncedBin`LSb(PTR_W));
-wire ptrsUnequalR = (rptrBin`LSb(PTR_W) != wptrSyncedBin`LSb(PTR_W));
-wire ptrsWrappedW = (wptrBin[PTR_W] != rptrSyncedBin[PTR_W]);
-wire ptrsWrappedR = (rptrBin[PTR_W] != wptrSyncedBin[PTR_W]);
+      .i_bit      (wptrGray_q[i]),
+
+      .o_bit      (wptrSyncedGray[i]),
+      .o_edge     (),
+      .o_rise     (),
+      .o_fall     (),
+      .o_nEdge    (),
+      .o_nRise    (),
+      .o_nFall    ()
+    );
+  end
+end : rDomain endgenerate
+
 
 generate if (FLOPS_NOT_MEM != 0) begin : useFlops
 
@@ -168,52 +226,17 @@ end : useFlops else begin : useMem
 
 end : useMem endgenerate
 
-generate for (i = 0; i <= PTR_W; i=i+1) begin
-  syncBit #(
-    .DEBOUNCE_CYCLES  (0),
-    .EDGECNTR_W       (1),
-    .N_SYNC           (2)
-  ) syncBit_wptr (
-    .i_clk      (i_rclk),
-    .i_cg       (i_rcg),
-    .i_rst      (i_rrst),
-
-    .i_bit      (wptrGray_q[i]),
-
-    .o_bit      (wptrSyncedGray[i]),
-    .o_edge     (),
-    .o_rise     (),
-    .o_fall     (),
-    .o_nEdge    (),
-    .o_nRise    (),
-    .o_nFall    ()
-  );
-  syncBit #(
-    .DEBOUNCE_CYCLES  (0),
-    .EDGECNTR_W       (1),
-    .N_SYNC           (2)
-  ) syncBit_rptr (
-    .i_clk      (i_wclk),
-    .i_cg       (i_wcg),
-    .i_rst      (i_wrst),
-
-    .i_bit      (rptrGray_q[i]),
-
-    .o_bit      (rptrSyncedGray[i]),
-    .o_edge     (),
-    .o_rise     (),
-    .o_fall     (),
-    .o_nEdge    (),
-    .o_nRise    (),
-    .o_nFall    ()
-  );
-end endgenerate
-
 `ifndef SYNTHESIS
 `dff_upcounter(reg [31:0], nPushed, i_wclk, i_wcg && o_wpushed, i_wrst)
 `dff_upcounter(reg [31:0], nPopped, i_rclk, i_rcg && o_rpopped, i_rrst)
 int signed nDiff = nPushed_q - nPopped_q;
-wire tooManyPush = nDiff > (TOPOLOGY ? DEPTH+1 : DEPTH);
+wire tooManyPush;
+generate if ((TOPOLOGY & 1) != 0)
+  // Extra stripes of flops on fastLarge can take another entry.
+  assign tooManyPush = nDiff > (DEPTH+1);
+else
+  assign tooManyPush = nDiff > DEPTH;
+endgenerate
 wire tooManyPop = nPopped_q > nPushed_q;
 `asrtw_en (!tooManyPush, i_wclk, i_wcg)
 `asrtw_en (!tooManyPop, i_rclk, i_rcg)
