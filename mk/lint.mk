aaf79b1a1d26b74869c494566e63facec858bce2
Dave McEwan <dave.mcewan@bristol.ac.uk>
2021-03-19 15:34:47 +0000

    tb/fxcs

--- mk/lint.mk
+++ mk/lint.mk
@@ -27,25 +27,26 @@ IVERILOG_SRC_SINGLEHIER ?= $(filter-out $(IVERILOG_SRC_EXCLUDE),$(SRC_SINGLEHIER
 IVERILOG_SRC_MULTIHIER ?= $(filter-out $(IVERILOG_SRC_EXCLUDE),$(SRC_MULTIHIER))
 IVERILOG_INCDIRS ?= $(INCDIRS)
 IVERILOG_LANG ?= -g2005-sv
-IVERILOG_FLAGS := $(IVERILOG_LANG) -o /dev/null $(addprefix -I,$(IVERILOG_INCDIRS))
+IVERILOG_LINT_FLAGS := $(IVERILOG_LANG) -o /dev/null $(addprefix -I,$(IVERILOG_INCDIRS))
 lint_iverilog: lint_iverilog_singlehier
 lint_iverilog: lint_iverilog_multihier
 lint_iverilog_singlehier: $(addprefix lint_iverilog_singlehier/,$(IVERILOG_SRC_SINGLEHIER))
 lint_iverilog_multihier:  $(addprefix lint_iverilog_multihier/,$(IVERILOG_SRC_MULTIHIER))
 lint_iverilog_singlehier/%:
-	iverilog $(IVERILOG_FLAGS) $*
+	iverilog $(IVERILOG_LINT_FLAGS) $*
 lint_iverilog_multihier/%:
-	iverilog $(IVERILOG_FLAGS) -i $*
+	iverilog $(IVERILOG_LINT_FLAGS) -i $*
 .PHONY: lint_iverilog lint_iverilog_singlehier lint_iverilog_multihier
 
 # Two-state simulator via C++ from Wilson Snyder.
 VERILATOR_SRC ?= $(filter-out $(VERILATOR_SRC_EXCLUDE),$(SRC_SINGLEHIER) $(SRC_MULTIHIER))
 VERILATOR_INCDIRS ?= $(INCDIRS)
 VERILATOR_LANG ?= --language 1800-2005
-VERILATOR_FLAGS := $(VERILATOR_LANG) $(addprefix -I,$(VERILATOR_INCDIRS))
+VERILATOR_LINT_FLAGS := $(VERILATOR_LANG) $(addprefix -I,$(VERILATOR_INCDIRS))
+VERILATOR_LINT_FLAGS += --lint-only
 lint_verilator: $(addprefix lint_verilator/,$(VERILATOR_SRC))
 lint_verilator/%:
-	verilator --lint-only $(VERILATOR_FLAGS) $*
+	verilator $(VERILATOR_LINT_FLAGS) $*
 .PHONY: lint_verilator
 
 # Generic synthesis from Clifford Wolf.
@@ -63,12 +64,12 @@ lint_yosys/%:
 FORMALPRO_SRC ?= $(filter-out $(FORMALPRO_SRC_EXCLUDE),$(SRC_SINGLEHIER) $(SRC_MULTIHIER))
 FORMALPRO_INCDIRS ?= $(INCDIRS)
 FORMALPRO_LANG ?= -sv2005
-FORMALPRO_FLAGS := $(FORMALPRO_LANG) +define+SYNTHESIS
-FORMALPRO_FLAGS += $(addprefix +incdir+,$(FORMALPRO_INCDIRS))
-FORMALPRO_LOGFILE = $(LOG_PREFIX)formalpro$(LOG_SUFFIX)
+FORMALPRO_LINT_FLAGS := $(FORMALPRO_LANG) +define+SYNTHESIS
+FORMALPRO_LINT_FLAGS += $(addprefix +incdir+,$(FORMALPRO_INCDIRS))
+FORMALPRO_LINT_LOGFILE = $(LOG_PREFIX)formalpro$(LOG_SUFFIX)
 lint_formalpro: $(addprefix lint_formalpro/,$(FORMALPRO_SRC))
 lint_formalpro/%:
-	formalpro $(FORMALPRO_FLAGS) $* > $(FORMALPRO_LOGFILE)
+	formalpro $(FORMALPRO_LINT_FLAGS) $* > $(FORMALPRO_LINT_LOGFILE)
 .PHONY: lint_formalpro
 
 # Linter from Cadence.
@@ -79,6 +80,7 @@ lint_formalpro/%:
 HAL_SRC ?= $(filter-out $(HAL_SRC_EXCLUDE),$(SRC_SINGLEHIER) $(SRC_MULTIHIER))
 HAL_INCDIRS ?= $(INCDIRS)
 HAL_LANG ?= -SV
+HAL_LINT_LOGFILE = $(LOG_PREFIX)hal$(LOG_SUFFIX)
 HAL_NOCHECK := LCVARN UCCONN VERCAS NUMSUF MAXLEN SEPLIN
 HAL_NOCHECK += UNMGEN NBGEND NOBLKN
 HAL_NOCHECK += STYVAL PRMVAL IDLENG
@@ -97,18 +99,16 @@ HAL_NOWARN += FFWNSR FFWASR
 HAL_NOWARN += VLGMEM DFDRVS
 HAL_NOWARN += TRUNCZ LRGOPR RDOPND SHFTOF REVROP MULBAS IOCOMB
 HAL_NOWARN += IMPDTC IMPTYP
-HAL_FLAGS := $(HAL_LANG) -DATE -DEFINE SYNTHESIS
-HAL_FLAGS += $(addprefix -NOCHECK ,$(HAL_NOCHECK))
-HAL_FLAGS += $(addprefix -NOWARN ,$(HAL_NOWARN))
-HAL_FLAGS += $(addprefix -INCDIR ,$(HAL_INCDIRS))
-HAL_LOGFILE = $(LOG_PREFIX)hal$(LOG_SUFFIX)
+HAL_LINT_FLAGS := $(HAL_LANG) -DATE -DEFINE SYNTHESIS
+HAL_LINT_FLAGS += $(addprefix -NOCHECK ,$(HAL_NOCHECK))
+HAL_LINT_FLAGS += $(addprefix -NOWARN ,$(HAL_NOWARN))
+HAL_LINT_FLAGS += $(addprefix -INCDIR ,$(HAL_INCDIRS))
+HAL_LINT_FLAGS += -LOGFILE $(HAL_LINT_LOGFILE)
+HAL_LINT_FLAGS += -DESIGN_FACTS_FILE $(LOG_PREFIX)hal_design_facts
 lint_hal: $(addprefix lint_hal/,$(HAL_SRC))
 lint_hal/%:
 	mkdir -p $(BUILD)
-	hal $(HAL_FLAGS) $* \
-		-LOGFILE $(HAL_LOGFILE) \
-		-DESIGN_FACTS_FILE $(LOG_PREFIX)hal_design_facts \
-		> /dev/null
+	hal $(HAL_LINT_FLAGS) $(HAL_FLAGS) $* > /dev/null
 .PHONY: lint_hal
 
 # Formal proof assistant from Cadence.
@@ -136,12 +136,12 @@ lint_jaspergold:
 # NOTE: Questa is the premium edition.
 MODELSIM_SRC ?= $(filter-out $(MODELSIM_SRC_EXCLUDE),$(SRC_SINGLEHIER) $(SRC_MULTIHIER))
 MODELSIM_LANG ?= -sv05compat
-MODELSIM_FLAGS := $(MODELSIM_LANG) -lint -quiet
-QVERILOG_LOGFILE = $(LOG_PREFIX)qverilog$(LOG_SUFFIX)
+MODELSIM_LINT_FLAGS := $(MODELSIM_LANG) -lint -quiet
+QVERILOG_LINT_LOGFILE = $(LOG_PREFIX)qverilog$(LOG_SUFFIX)
 lint_modelsim: $(addprefix lint_modelsim/,$(MODELSIM_SRC))
 lint_modelsim/%:
-	vlog $(MODELSIM_FLAGS) $*
-	qverilog $(MODELSIM_FLAGS) $* > $(QVERILOG_LOGFILE)
+	vlog $(MODELSIM_LINT_FLAGS) $*
+	qverilog $(MODELSIM_LINT_FLAGS) $* > $(QVERILOG_LINT_LOGFILE)
 .PHONY: lint_modelsim
 
 # Linter from Synopsys.
@@ -155,11 +155,11 @@ read_file $(SPYGLASS_SRC)
 endef
 export SPYGLASS_TCL
 SPYGLASS_TCLFILE ?= $(BUILD)/spyglass-mk_lint.prj
-SPYGLASS_LOGFILE = $(BUILD)/spyglass$(LOG_SUFFIX)
+SPYGLASS_LINT_LOGFILE = $(BUILD)/spyglass$(LOG_SUFFIX)
 lint_spyglass:
 	mkdir -p $(BUILD)
 	@echo "$$SPYGLASS_TCL" > $(SPYGLASS_TCLFILE)
-	spyglass -batch -designread -project $(SPYGLASS_TCLFILE) > $(SPYGLASS_LOGFILE)
+	spyglass -batch -designread -project $(SPYGLASS_TCLFILE) > $(SPYGLASS_LINT_LOGFILE)
 .PHONY: lint_spyglass
 
 # RTL simulator from Synopsys.
@@ -167,11 +167,11 @@ lint_spyglass:
 VCS_SRC ?= $(filter-out $(VCS_SRC_EXCLUDE),$(SRC_SINGLEHIER) $(SRC_MULTIHIER))
 VCS_INCDIRS ?= $(INCDIRS)
 VCS_LANG ?= -sverilog
-VCS_FLAGS := $(VCS_LANG) +lint=all -C -q $(addprefix +incdir+,$(VCS_INCDIRS))
-VCS_LOGFILE ?= $(BUILD)/vcs-mk_lint.log
+VCS_LINT_FLAGS := $(VCS_LANG) +lint=all -C -q $(addprefix +incdir+,$(VCS_INCDIRS))
+VCS_LINT_LOGFILE ?= $(BUILD)/vcs-mk_lint.log
 lint_vcs:
 	mkdir -p $(BUILD)
-	vcs $(VCS_FLAGS) $(VCS_SRC) > $(VCS_LOGFILE)
+	vcs $(VCS_LINT_FLAGS) $(VCS_SRC) > $(VCS_LINT_LOGFILE)
 
 # FPGA bitstream compiler from Xilinx.
 # Tested with Vivado 2018.2, 2018.3
@@ -184,7 +184,7 @@ check_syntax -fileset filesetDummy
 endef
 export VIVADO_TCL
 VIVADO_TCLFILE ?= $(BUILD)/vivado-mk_lint.tcl
-VIVADO_LOGFILE = $(BUILD)/vivado$(LOG_SUFFIX)
+VIVADO_LINT_LOGFILE = $(BUILD)/vivado$(LOG_SUFFIX)
 lint_vivado:
 	rm -f vivado*.jou
 	rm -f vivado*.log
@@ -192,9 +192,9 @@ lint_vivado:
 	mkdir -p $(BUILD)
 	@echo "$$VIVADO_TCL" > $(VIVADO_TCLFILE)
 	vivado -mode batch -source $(VIVADO_TCLFILE) > /dev/null
-	mv vivado.log $(VIVADO_LOGFILE)
-	! grep -q ERROR $(VIVADO_LOGFILE)
-	! grep -q WARNING $(VIVADO_LOGFILE)
+	mv vivado.log $(VIVADO_LINT_LOGFILE)
+	! grep -q ERROR $(VIVADO_LINT_LOGFILE)
+	! grep -q WARNING $(VIVADO_LINT_LOGFILE)
 .PHONY: lint_vivado
 
 # RTL simulator from Cadence, successor to Incisive.
@@ -202,12 +202,12 @@ lint_vivado:
 XMVLOG_SRC ?= $(filter-out $(XMVLOG_SRC_EXCLUDE),$(SRC_SINGLEHIER) $(SRC_MULTIHIER))
 XMVLOG_INCDIRS ?= $(INCDIRS)
 XMVLOG_LANG ?= -SYSV05
-XMVLOG_FLAGS := $(XMVLOG_LANG) -NOLOG $(addprefix -INCDIR ,$(XMVLOG_INCDIRS))
-XMVLOG_LOGFILE = $(LOG_PREFIX)xmvlog$(LOG_SUFFIX)
+XMVLOG_LINT_FLAGS := $(XMVLOG_LANG) -NOLOG $(addprefix -INCDIR ,$(XMVLOG_INCDIRS))
+XMVLOG_LINT_LOGFILE = $(LOG_PREFIX)xmvlog$(LOG_SUFFIX)
 lint_xmvlog: $(addprefix lint_xmvlog/,$(XMVLOG_SRC))
 lint_xmvlog/%:
 	mkdir -p $(BUILD)
-	xmvlog $(XMVLOG_FLAGS) $* > $(XMVLOG_LOGFILE)
+	xmvlog $(XMVLOG_LINT_FLAGS) $* > $(XMVLOG_LINT_LOGFILE)
 
 
 CLEAN_PATHS += work
--- mk/preproc.mk
+++ mk/preproc.mk
@@ -5,10 +5,11 @@ PREPROC_SRC ?= $(SRC_SINGLEHIER) $(SRC_MULTIHIER)
 
 VERILATOR_INCDIRS ?= $(INCDIRS)
 VERILATOR_LANG ?= --language 1800-2005
+VERILATOR_PREPROC_FLAGS := $(VERILATOR_LANG) $(addprefix -I,$(VERILATOR_INCDIRS))
+VERILATOR_PREPROC_FLAGS += -E -P -Mdir $(BUILD)/obj_dir
+
 preproc: $(addprefix $(BUILD)/preproc/,$(PREPROC_SRC))
 $(BUILD)/preproc/%:
 	mkdir -p $(shell dirname $@)
-	verilator -E -P -Mdir $(BUILD)/obj_dir $(VERILATOR_LANG) \
-		$(addprefix -I,$(VERILATOR_INCDIRS)) $* \
-		> $(BUILD)/preproc/$(notdir $*)
+	verilator $(VERILATOR_PREPROC_FLAGS) $* > $(BUILD)/preproc/$(notdir $*)
 
