dd377d25ebe8b49f5648103be46329d981ce2c3b
Dave McEwan <dave.mcewan@bristol.ac.uk>
2021-03-26 18:19:11 +0000

    corrOPA Collect results of linux running idle.

--- prj/minProtoUst_VC707/corrdemo0_test0.py
+++ prj/minProtoUst_VC707/corrdemo0_test0.py
@@ -6,6 +6,7 @@ from __future__ import division
 
 import sys
 import time
+import traceback
 
 if __name__ == "__main__":
     print("Not a standalone module.")
@@ -13,26 +14,89 @@ if __name__ == "__main__":
 
 #from dmppl.base import verb, dbg
 
-from test_env import send_msg, recv_msg
-from ust_evaluation import configureMessageInfrastructure, \
-    discoverStatusMonitor, receiveStatusMonCounterDataMessages
+from test_env import send_msg, recv_msg, codec
+from ust_evaluation import TargetDisconnected, \
+    configureMessageInfrastructure, disableModules, discoverStatusMonitor, \
+    receiveStatusMonCounterDataMessages
 
-# NOTE: test_params is used by receiveStatusMonCounterDataMessages().
-if "sample_limit" not in test_params:
-    test_params["sample_limit"] = 5
-sample_limit = int(test_params["sample_limit"])
-
-
-fnameo = "test0.csv"
 nQualifiers = 16
 
 windowLengthExp     = 15        # 32k samples
-windowShape         = "logdrop" # 1'd1
+windowShape         = "rectangular" # 1'd1
 samplePeriodExp     = 0         # Lockstep with interval-timer.
 sampleJitterExp     = 0         # Jitter not supported because of interval-timer message-push mechanism.
-pwmSelect           = 5         # Cov
-probeX              = 10        # 22    <-->    10
-probeY              = 2         # 5     <-->    2
+pwmSelect           = 0         # winNum
+
+if "fnameo" not in test_params:
+    test_params["fnameo"] = "test0.csv"
+fnameo = str(test_params["fnameo"])
+
+if "nSamples" not in test_params:
+    test_params["nSamples"] = 1000
+nSamples = int(test_params["nSamples"])
+
+if "probeX" not in test_params:
+    test_params["probeX"] = 10      # 22    <-->    10
+probeX = int(test_params["probeX"])
+
+if "probeY" not in test_params:
+    test_params["probeY"] = 2       # 5     <-->    2
+probeY = int(test_params["probeY"])
+
+def receiveStatusMonCorrelatorMessages(target_sm, discovery, output): # {{{
+
+  global sw_link_disconnected
+
+  assert 0 == discovery.fields['accumulator_width'].value
+  assert 1 == discovery.fields['counters'].value
+  assert 32 == {1:16, 2:24, 3:32, 4:48, 5:64}[discovery.fields['counter_width'].value]
+
+  def is_monitor_type_data(msg_type, msg):
+      if codec.msg_generation < 2:
+          return msg.header == 'monitor_data'   and msg.fields['type'] == 'monitor_'+msg_type+'_data'
+      else:
+          return msg.header == 'system_control' and msg.fields['msg_type'] == 'monitor_'+msg_type+'_data'
+
+  output.write("x, y, x_isect_y, x_symdiff_y\n")
+
+  sample = 0
+  while True:
+      try:
+
+        m = recv_msg()
+
+        if m.module == target_sm and is_monitor_type_data('counter', m):
+            assert 0 == m.fields['cause'].value # Interval timer expiry
+            pkt = m.fields['counters'].value
+            x, y, x_isect_y, x_symdiff_y = (
+                ((pkt >> (0*8)) & 0xff),
+                ((pkt >> (1*8)) & 0xff),
+                ((pkt >> (2*8)) & 0xff),
+                ((pkt >> (3*8)) & 0xff),
+            )
+            output.write("%d,%d,%d,%d\n" % (x, y, x_isect_y, x_symdiff_y))
+            sample += 1
+
+            if not (x == y == x_isect_y) or not (0 == x_symdiff_y):
+                print("LOOK HERE:", sample, x, y, x_isect_y, x_symdiff_y)
+
+            if sample >= nSamples:
+                print("Ending test: sample limit reached...")
+                disableModules()
+                break
+
+      except TargetDisconnected:
+        # Return control to test script if sw-link disconnects
+        print("Ending test: sw-link disconnected...")
+        sw_link_disconnected = True
+        raise TargetDisconnected("test script ending")
+        break
+      except Exception as e:
+        print("Unexpected exception [%s]" % (e.__class__.__name__))
+        traceback.print_exc()
+        sys.exit()
+# }}} def receiveStatusMonCorrelatorMessages
+
 
 # Configure the UltraDebug message infrastructure and return handles to the
 # message engine and status monitor.
@@ -157,7 +221,7 @@ send_msg(
         "msg_type"              : "sc",                 # 2'b00
         "control_code"          : "set_period",         # 6'h1e
         "timer_enable"          : 1,                    # 2'd1 Continuous
-        "timer_interval"        : 1 << windowLengthExp, # 64'
+        "timer_interval"        : 2**(windowLengthExp+samplePeriodExp), # 64'
         "timer_index_trigger"   : 0,                    # 8'd0
     }
 )
@@ -300,8 +364,8 @@ send_msg(
 recv_msg()
 # }}} Enable module and all qualifiers. (UG 5.1.16)
 
-with open(fnameo, 'w') as fd:
-    receiveStatusMonCounterDataMessages(sm, discovery, fd)
+with open("../../"+fnameo, 'w') as fd:
+    receiveStatusMonCorrelatorMessages(sm, discovery, fd)
 
 # {{{ Finally disable module and all qualifiers. (UG 5.1.16)
 send_msg(
