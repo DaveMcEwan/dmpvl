0bb1798076a7a3b2ab38648fa7033a230d81c3a4
Dave McEwan <638426+DaveMcEwan@users.noreply.github.com>
2022-03-09 10:52:56 +0000

    style_commaleading Tidy, corrections, add module instances.

--- prs/shr/sv/style_commaleading.sv
+++ prs/shr/sv/style_commaleading.sv
@@ -2,7 +2,19 @@
 <https://gist.github.com/isaacs/357981>
 <https://github.com/tibbe/haskell-style-guide/blob/master/haskell-style.md#list-declarations>
 */
-module M
+
+/* Module declaration without parameter ports.
+*/
+module Mod_A
+( input  var logic i_abc // comment
+, inout  var logic b_def /* comment */
+, output var logic o_ghi
+);
+endmodule
+
+/* Module declaration with parameter ports.
+*/
+module Mod_B
 #(int FOO = 1 // comment
 , bit BAR = 2 /* comment */
 , bit [31:0] BAZ = 2
@@ -14,9 +26,9 @@ module M
 );
 
 /* Each item on its own line.
-Nicest for readability:
-- short lines
-- every list indented to same level
+- Short lines.
+- Every list indented to same level.
+- Single-line LHS can be any length without indent issue.
 */
 assign {foo, bar} =
   { i_abc
@@ -25,48 +37,31 @@ assign {foo, bar} =
   , 16'h3456  /* comment */
   };
 
+/* First item on same line.
+*/
+assign lessnice_A = { i_abc // No indent alignment issue.
+                    , 12'h345
+                    , b_def
+                    , 16'h3456
+                    };
+assign lessnice_B_ =  { i_abc // Extra space after `=` to align with indent.
+                      , 12'h345
+                      , b_def
+                      , 16'h3456
+                      };
+
 /* Everything fits on one line.
-Omit space after opening parenthesis/bracket/brace.
+- No space after opening parenthesis/bracket/brace.
 */
 assign singleline1D = {i_abc, 12'h345, b_def, 16'h3456};
 assign singleline2D = {{foo, bar}, {foo, bar}, {foo, bar}};
 
-/* First item on same line.
-No indent alignment issue.
-*/
-assign foobar = { i_abc
-                , 12'h345
-                , b_def
-                , 16'h3456
-                };
-
-/* First item on same line, again.
-Extra space after `=` to align with indent.
-*/
-assign foo =  { i_abc
-              , 12'h345
-              , b_def
-              , 16'h3456
-              };
-
-/* Multi-dimensional concatenation
-Innermost array on one line.
+/* Multi-dimensional concatenation with innermost array on one line.
 */
 assign matrix2D_A =
   { {elem21, elem20}
   , {elem11, elem10} // comment
   , {elem01, elem00} /* comment */
-  };
-assign matrix2D_B =
-  { { elem21
-    , elem20
-    }
-  , { elem11 // comment
-    , elem10 /* comment */
-    }
-  , { elem01
-    , elem00
-    }
   };
 assign matrix3D_A =
   { { {elem211, elem210}
@@ -79,7 +74,21 @@ assign matrix3D_A =
     , {elem001, elem000}
     }
   };
-assign matrix3D_A =
+
+/* Multi-dimensional concatenation with innermost array on multiple lines.
+*/ 
+assign matrix2D_B =
+  { { elem21
+    , elem20
+    }
+  , { elem11 // comment
+    , elem10 /* comment */
+    }
+  , { elem01
+    , elem00
+    }
+  };
+assign matrix3D_B =
   { { { elem211
       , elem210
       }
@@ -102,5 +111,25 @@ assign matrix3D_A =
       }
     }
   };
+
+/* Module instance without parameter ports.
+*/
+Mod_A u_Mod_A0
+( .i_abc(foo) // comment
+, .b_def({bar, bar}) /* comment */
+, .o_ghi
+);
+
+/* Module instance with parameter ports.
+*/
+Mod_B
+#(.FOO(1) // comment
+, .BAR(2) /* comment */
+, .BUZZ(2)
+) u_Mod_B
+( .i_abc(foo) // comment
+, .b_def({bar, bar}) /* comment */
+, .o_ghi
+);
 
 endmodule
