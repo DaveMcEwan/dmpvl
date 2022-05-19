d52231db7ce615a441e1636c27a2c6683d849471
damc <dave.mcewan@nordicsemi.no>
2022-05-19 13:31:39 +0200

    csv.mk Add more ROWMAP templates and CSV_COL function.

--- mk/csv.mk
+++ mk/csv.mk
@@ -24,9 +24,10 @@ N_CSV_KEYS := $(shell echo $(CSV_KEYS) | wc -w)
 CSV_ROWS := $(shell $(CSV_LINEPARSE) $(CSV) | tail -n+2 | $(CSV_COLPARSE))
 N_CSV_ROWS := $(shell echo $(CSV_ROWS) | wc -w)
 
-# Callable macro to map each row in CSV to a template.
+# Function to map each row in CSV to a template.
 # Use like $(call CSV_ROWMAP,<csv row>,<template using $KEY and $VALUE>)
-#   E.g. $(call CSV_ROWMAP,$*,-D$$KEY=$$VALUE)
+#   E.g.
+#     $(call CSV_ROWMAP,$*,-D$$KEY=$$VALUE)
 # NOTE: Depends on Bash syntax. Csh could be supported with ifdef in future.
 CSV_ROWMAP = \
 	$(shell for i in $$(seq 1 $(N_CSV_KEYS)); do \
@@ -35,7 +36,32 @@ CSV_ROWMAP = \
 		echo $(2); \
   done)
 
-# Convenience templates.
-# Map each row in CSV to a preprocessor define (-Dkey=value).
+# Convenience template(s) map each row in CSV to a useful format.
+#   E.g.
+#     $(call CSV_ROWMAP,$*,${CSV_ROWMAP_CFLAGDEFINE})
+#   E.g.
+#     JSONS := $(foreach row,${CSV_ROWS},{ $(call CSV_ROWMAP,${row},${CSV_ROWMAP_JSON}) })
+# 1. CFLAGS-style preprocessor define (-Dkey=value).
 CSV_ROWMAP_CFLAGDEFINE ?= -D$$KEY=$$VALUE
+# 2. Short-format argument (-key value).
+CSV_ROWMAP_SHORTARG ?= -$$KEY $$VALUE
+# 3. Long-format argument (--key=value).
+CSV_ROWMAP_LONGARG ?= --$$KEY=$$VALUE
+# 4. JSON dictionary item ("key": "value",).
+CSV_ROWMAP_JSON ?= \"$$KEY\": \"$$VALUE\",
+
+# See the trick for commas and spaces here:
+#   https://www.gnu.org/software/make/manual/make.html#Syntax-of-Functions
+comma := ,
+empty :=
+space := $(empty) $(empty)
+
+# Function to get a value from a single row by column index, starting at 1.
+#   E.g. Where ${row} is "no,no,no,no,yes,no" this gives "yes".
+#     $(call CSV_COL,5,${row})
+# This can also be used to define convenience functions
+#   E.g. Where the 5th column holds names, create a list of those names.
+#     R_NAME = $(call CSV_COL,5,${row})
+#     NAMES := $(foreach row,${CSV_ROWS},$(call R_NAME))
+CSV_COL = $(word $(1),$(subst $(comma),$(space),$(2)))
 
